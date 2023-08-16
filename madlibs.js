a71e3660887f92dd13527ad121fb77d2f3b324be
mohamed <matassi.mohamed89@gmail.com>
2023-08-16 12:13:01 +0100

    workig with js tasks

--- madlibs.js
+++ madlibs.js
@@ -26,9 +26,29 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
+const rawStory = '[n] [v] to the [n], and it was [a]. He [v] the [n].';
+
 function parseStory(rawStory) {
   // Your code here.
-  return {}; // This line is currently wrong :)
+  const storyParts = rawStory.split(/(\[n\]|\[a\]|\[v\])/);
+  const processedStory = [];
+
+  for (let i = 0; i < storyParts.length; i++) {
+    const word = storyParts[i];
+    if (word === "[n]") {
+      processedStory.push({ word: "", pos: "noun" });
+    } else if (word === "[v]") {
+      processedStory.push({ word: "", pos: "verb" });
+    } else if (word === "[a]") {
+      processedStory.push({ word: "", pos: "adjective" });
+    } else {
+      processedStory.push({ word });
+    }
+  }
+  console.log(rawStory);
+
+  return processedStory;
+  // return {}; // This line is currently wrong :)
 }
 
 /**
@@ -37,6 +57,30 @@ function parseStory(rawStory) {
  * 
  * You'll want to use the results of parseStory() to display the story on the page.
  */
+
 getRawStory().then(parseStory).then((processedStory) => {
+  const editView = document.getElementsByClassName("madLibsEdit")[0];
+  const previewView = document.getElementsByClassName("madLibsPreview")[0];
+
+  processedStory.forEach((wordObj) => {
+    const span = document.createElement("span");
+
+    if (wordObj.pos) {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.value = wordObj.word;
+      input.addEventListener("input", () => {
+        span.textContent = input.value;
+      });
+
+      span.appendChild(input);
+      previewView.appendChild(document.createTextNode(`[${wordObj.pos}]`));
+    } else {
+      span.textContent = wordObj.word;
+      previewView.appendChild(document.createTextNode(wordObj.word));
+    }
+
+    editView.appendChild(span);
+  });
   console.log(processedStory);
 });
