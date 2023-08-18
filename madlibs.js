580354a433c13b9431e99701cd3ded658b19cdc2
Hemsas Riane <rayan.hemsas@gmail.com>
2023-08-18 16:05:00 +0200

    new changes css html

--- madlibs.js
+++ madlibs.js
@@ -26,10 +26,14 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
-function parseStory(rawStory) {
+
+
+
+// function parseStory(rawStory) {
   // Your code here.
-  return {}; // This line is currently wrong :)
-}
+  // return {};
+   // This line is currently wrong :)
+// }
 
 /**
  * All your other JavaScript code goes here, inside the function. Don't worry about
@@ -37,6 +41,8 @@ function parseStory(rawStory) {
  * 
  * You'll want to use the results of parseStory() to display the story on the page.
  */
-getRawStory().then(parseStory).then((processedStory) => {
-  console.log(processedStory);
-});
+
+
+// getRawStory().then(parseStory).then((processedStory) => {
+//   console.log(processedStory);
+// });
