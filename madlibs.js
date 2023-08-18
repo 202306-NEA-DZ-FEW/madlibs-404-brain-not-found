9c663deba32034c976d7c3b5025a37480c18731d
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-08-18 16:40:51 +0100

    using span in the preview div + hide span content + live preview event + reset button

--- madlibs.js
+++ madlibs.js
@@ -1,33 +1,3 @@
-/**
- * Complete the implementation of parseStory.
- * 
- * parseStory retrieves the story as a single string from story.txt
- * (I have written this part for you).
- * 
- * In your code, you are required (please read this carefully):
- * - to return a list of objects
- * - each object should definitely have a field, `word`
- * - each object should maybe have a field, `pos` (part of speech)
- * 
- * So for example, the return value of this for the example story.txt
- * will be an object that looks like so (note the comma! periods should
- * be handled in the same way).
- * 
- * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
- * Output: [
- *  { word: "Louis", pos: "noun" },
- *  { word: "went", pos: "verb", },
- *  { word: "to", },
- *  { word: "the", },
- *  { word: "store", pos: "noun" }
- *  { word: "," }
- *  ....
- * 
- * There are multiple ways to do this, but you may want to use regular expressions.
- * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
- */
-// const rawStory = '[n] [v] to the [n], and it was [a]. He [v] the [n].';
-
 function parseStory(rawStory) {
   // Your code here.
   const updatedRawStory = rawStory.replace(/(\w+)\s*(\[n\]|\[a\]|\[v\])/g, '$2');
@@ -54,69 +24,13 @@ function parseStory(rawStory) {
   // return {}; // This line is currently wrong :)
 }
 
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- * 
- * You'll want to use the results of parseStory() to display the story on the page.
- */
-
-// getRawStory().then(parseStory).then((processedStory) => {
-//   const editView = document.getElementsByClassName("madLibsEdit")[0];
-//   const previewView = document.getElementsByClassName("madLibsPreview")[0];
-
-//   processedStory.forEach((wordObj) => {
-//     const span = document.createElement("span");
-//     // span.setAttribute = ('for', '')
-
-//     if (wordObj.pos) {
-//       // const input = document.createElement("input");
-//       // input.type = "text";
-//       // input.name = 
-//       // input.setAttribute = ('placeholder', 'word');
-//       // input.value = wordObj.word;
-//       // input.addEventListener("input", () => {
-//       //   span.textContent = input.value;
-//       // });
-//       for (let i = 0; i < processedStory.length; i++) {
-//         const input = document.createElement('input');
-//         input.type = "text";
-//         input.name = "input-" + i; // You can set a unique name for each input
-//         input.addEventListener("input", () => {
-//           span.textContent = input.value;
-//         });
-//         if (processedStory[i] === "[n]") {
-//             input.placeholder = "Enter a noun";
-//         } else if (processedStory[i] === "[v]") {
-//             input.placeholder = "Enter a verb";
-//         } else if (processedStory[i] === "[a]") {
-//             input.placeholder = "Enter an adjective";
-//         } else {
-//             input.value = processedStory[i];
-//         }
-//         document.getElementById('input-container').appendChild(input);
-//       }
-
-//       span.appendChild(input);
-//       previewView.appendChild(document.createTextNode(`[${wordObj.pos}]`));
-//     } else {
-//       span.textContent = wordObj.word;
-//       previewView.appendChild(document.createTextNode(wordObj.word));
-//     }
-
-//     editView.appendChild(span);
-//   });
-//   console.log(processedStory);
-// });
  document.addEventListener("DOMContentLoaded", () => {  
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
        //creating input
-    const editView = document.getElementsByClassName("madLibsEdit")[0];
-    const previewView = document.getElementsByClassName("madLibsPreview")[0];
-    const storyParts = processedStory.map((wordObj) => wordObj.word);
-    console.log(storyParts)
+       const editView = document.querySelector(".madLibsEdit");
+       const previewView = document.querySelector(".madLibsPreview");
     let j=0
     for (let i = 0; i < processedStory.length; i++) {
       const wordObj = processedStory[i];
@@ -126,36 +40,35 @@ getRawStory()
       if (wordObj.pos) {
         j++
         const input_edit = document.createElement("input");
-        const input_preview = document.createElement("input");
         input_edit.type = "text";
         input_edit.maxLength = '20';
         input_edit.name = "input-" + j;
-        input_preview.type = "text";
-        input_preview.maxLength = '20';
-        input_preview.name = "input-" + j;
-        input_preview.readOnly="true"
-        input_edit.addEventListener("keydown", (e) => {
-          input_preview.value=e.target.value
-        });
-
+        span_preview.id="input-" + j;
+        
+    
         if (wordObj.pos === "noun") {
           input_edit.placeholder = "noun";
-          input_preview.placeholder = "noun";
-        } else if (wordObj.pos === "verb") {
+         span_preview.textContent="(noun)"
+         span_preview.style.visibility="hidden"
+        }
+        
+        else if (wordObj.pos === "verb") {
           input_edit.placeholder = "verb";
-          input_preview.placeholder = "verb";        } 
+          span_preview.textContent= "(verb)"
+          span_preview.style.visibility="hidden"
+           } 
+
+
           else if (wordObj.pos === "adjective") {
             input_edit.placeholder = "adjective";
-            input_preview.placeholder = "adjective"; 
+            span_preview.textContent = "(adjective)";
+            span_preview.style.visibility="hidden"
                   }
         span_edit.appendChild(input_edit);
-        span_preview.appendChild(input_preview)
       }
        else {
         span_edit.textContent = wordObj.word;
-        console.log(span_edit)
         span_preview.textContent=wordObj.word;
-        console.log(previewView)
       }
 
       editView.appendChild(span_edit);
@@ -163,12 +76,92 @@ getRawStory()
     }
              
   })
-  // //detect change in input
-  // const detect_change=(element)=>{
-  //   if(element.name==="input-1"){
-  //   }
-  // }
-  // document.addEventListener('keydown',(event)=>{
-  //   console.log(event.target.name)
-  // })
+
+
+  // event
+  // live update
+  document.addEventListener("input", (e) => {
+    const input=e.target
+    const span=document.getElementById(input.name)
+    span.style.visibility = "visible"
+    span.textContent=input.value
+   });
+   
+
+
+   // reset button
+   const resetButton=document.createElement('button')
+   resetButton.textContent='Reset'
+   const body=document.querySelector('body')
+   body.appendChild(resetButton)
+   resetButton.addEventListener('click',()=>{
+   const inputs=document.querySelectorAll('input')
+   inputs.forEach((item)=>{
+   const span=document.querySelector(`#${item.name}`)
+   console.log(span)
+   span.textContent=`(${item.placeholder})`
+   span.style.visibility="hidden"
+   item.value=""
+  })
+   })
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
