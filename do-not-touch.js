6c228a8461e2b24796252f85887718a3826deb84
mohamed <matassi.mohamed89@gmail.com>
2023-08-19 16:10:04 +0100

    finale updates on JS

--- do-not-touch.js
+++
@@ -1,9 +0,0 @@
-/**
- * DO NOT TOUCH ANY OF THE CODE BELOW HERE.
- * 
- * Or you will be very sad.
- */
-const getRawStory = () => {
-  return fetch('./story.txt')
-    .then(response => response.text());
-};
